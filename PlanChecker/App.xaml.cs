a6d23138e48100508c00ebfb61f3aec53b985acf
Ryan Smith <ryansmith3765@hotmail.com>
2018-06-05 13:50:10 -0700

    Add project files.

---
+++ PlanChecker/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ESAPIX.Bootstrapper;
+using PlanChecker.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlanChecker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var bs = new AppBootstrapper<MainView>(() => { return VMS.TPS.Common.Model.API.Application.CreateApplication(); });
+            bs.Run();
+        }
+    }
+}
