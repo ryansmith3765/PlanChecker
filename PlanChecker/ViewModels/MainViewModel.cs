d2509d92084676ecdad1d13e9e38fea47ac7ad57
Ryan Smith <ryansmith3765@hotmail.com>
2018-06-05 15:14:21 -0700

    TEST

--- PlanChecker/ViewModels/MainViewModel.cs
+++ PlanChecker/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using ESAPIX.Interfaces;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PlanChecker.ViewModels
 {
@@ -17,17 +20,60 @@ namespace PlanChecker.ViewModels
             _ctx = ctx;
 
             //Example data bind
-            Username = ctx.CurrentUser.Id;
+            Result = "PASSED";
+
+            ButtonCommand = new DelegateCommand(() =>
+            {
+                // MessageBox.Show("PRESSED!");
+                //EValuate the plan
+                //convert thhe plan ID to the actual plan.
+                var selected = _ctx.PlansInScope.First(p => p.Id == SelectedPlan);
+                Result = SelectedPlan + "PASSED";
+            });
+            ctx.CourseChanged += Ctx_CourseChanged;
+
+            
         }
-
-
-        private string username;
-
-        public string Username
+        
+        private void Ctx_CourseChanged(ESAPIX.Facade.API.Course c)
         {
-            get { return username; }
-            set { SetProperty(ref username, value); }
+            FillPlans();
         }
 
+        private void FillPlans()
+        {
+            //Fills the list with plan Ids
+            if(_ctx.PlansInScope == null) { return; }
+            Plans.Clear(); 
+            foreach(var plan in _ctx.PlansInScope)
+            {
+                Plans.Add(plan.Id);
+            }
+        }
+
+        private string result;
+
+        public string Result
+        {
+            get { return result; }
+            set { SetProperty(ref result, value); }
+        }
+
+        public DelegateCommand ButtonCommand { get; set; }
+
+        //List for user interfaces
+        public ObservableCollection<string> Plans { get; set; } = new ObservableCollection<string>();
+
+        private string selectedplan;
+
+        public string SelectedPlan
+        {
+            get { return selectedplan; }
+            set { SetProperty(ref selectedplan, value); }
+        }
+
+       
     }
+
+
 }
